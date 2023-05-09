fd22c421e0f6416a6ffe4deb06ba7a18f650873e
Ali Khan <alikhan@Alis-MacBook-Pro.local>
2023-05-09 15:57:01 +0100

    ensured endpoint is working and passed all test cases

--- index.test.js
+++ index.test.js
@@ -8,15 +8,43 @@ const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
 const seedMusician = require("./seedData");
+const { TestWatcher } = require('jest');
 
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+    test('Testing Musicians endpoint', async () => {
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200);
+    })
     
+    test('Testing Musicicans name', async () => {
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(responseData[0]).toHaveProperty('name')
+    })
+
+    test('Testing Musicians instrument', async () => {
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(responseData[0]).toHaveProperty('instrument')
+    })
     
-
-
-
-
+    test('Testing Musicians correct name', async () => {
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(responseData[0].name).toBe('Mick Jagger')
+    })
     
+    test('Testing Musicians correct instrument', async () => {
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(responseData[0].instrument).toBe('Voice')
+    })
+    test('Testing Musicians ID', async () => {
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(responseData[0]).toHaveProperty('id')
+    })
+
 })
--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
+app.set("json spaces", 2);
+
+
 //TODO: Create a GET /musicians route to return all musicians 
+app.get('/musicians', async (req, res) => {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+})
 
-
-
-
+app.get('/band', async (req, res) => {
+    const bands = await Band.findAll();
+    res.json(bands);
+})
 
 
 
