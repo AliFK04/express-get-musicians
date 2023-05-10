4296eb362014800009c0ede791608f4f29a31373
AliFK04 <AliFK04>
2023-05-10 16:10:59 +0100

    yet to do the tests, but I tested it through Thunder-client and the sqlite databases and confirm that code works

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const { Musician, Band } = require("../models/index")
-const { db } = require("../db/connection")
+const { db } = require("../db/connection");
+const { urlencoded } = require("express");
 
 const port = 3000;
 
 app.set("json spaces", 2);
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 //TODO: Create a GET /musicians route to return all musicians 
 app.get('/musicians', async (req, res) => {
@@ -14,10 +17,28 @@ app.get('/musicians', async (req, res) => {
     res.json(musicians);
 })
 
-app.get('/band', async (req, res) => {
-    const bands = await Band.findAll();
-    res.json(bands);
+app.get('/musicians/:id', async (req, res) => {
+    const musicians = await Musician.findByPk(req.params.id);
+    res.json(musicians);
 })
+
+app.post('/musicians', async (req, res) => {
+    const musicians = await Musician.create(req.body);
+    res.json(musicians);
+})
+app.put('/musicians/:id', async (req, res) => {
+    const updMusician = await Musician.update(req.body, {where: {id: req.params.id}});
+    res.json(updMusician);
+})
+app.delete('/musicians/:id', async (req, res) => {
+    const deleteMusic = await Musician.destroy({where: {id: req.params.id}});
+    res.json(deleteMusic);
+})
+
+// app.get('/bands', async (req, res) => {
+//     const bands = await Band.findAll();
+//     res.json(bands);
+// })
 
 
 
